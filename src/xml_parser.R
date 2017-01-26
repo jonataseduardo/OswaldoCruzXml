37336af6ec462ca78d441c4ef0dd7b32d8204fe2
jonatas <jonataseduardo@gmail.com>
2017-01-26 20:40:26 -0200

    dissecting the file structure

--- src/xml_parser.R
+++ src/xml_parser.R
@@ -2,26 +2,43 @@ library(data.table)
 library(xml2)
 
 help(package = 'xml2')
+?xml_serialize
+
+
+list_with_na <-
+    lapply(L, function(x) ifelse(is.null(x), NA, x))
 
 raw_xml <- 
-  read_xml("../data/CD000004RawData.xml")
+  read_xml("../data/CD003407RawData.xml")
 
 RD_DATA <- xml_find_all(raw_xml, "//RD_DATA")
 RD_OUT <- xml_find_all(raw_xml, "//RD_OUT")
 
+
+p_RD_COMP <- "RD_COMP\\[([0-9]+)\\]"
 p_RD_OUT <- "RD_OUT\\[([0-9]+)\\]"
-p_RD_DATA <- "(RD_DATA\\[)([0-9]+)(\\]$)"
+p_RD_SUB <- "RD_SUB\\[([0-9]+)\\]"
+p_RD_DATA <- "RD_DATA\\[([0-9]+)\\]$"
 
 path_RD_DATA <- xml_path(RD_DATA)
+path_RD_DATA[1:50]
 
-r1 <- gregexpr(p_RD_DATA, path_RD_DATA)
-L1 <-   regmatches(path_RD_DATA, r1)
+r_comp <- gregexpr(p_RD_COMP, path_RD_DATA)
+l_comp <- regmatches(path_RD_DATA, r0)
 
-r2 <- gregexpr(p_RD_OUT, path_RD_DATA)
-L2 <-   regmatches(path_RD_DATA, r2)
+r_out <- gregexpr(p_RD_OUT, path_RD_DATA)
+l_out <- regmatches(path_RD_DATA, r2)
+
+r_sub <- gregexpr(p_RD_SUB, path_RD_DATA)
+l_sub <- regmatches(path_RD_DATA, r2)
+
+r_data <- gregexpr(p_RD_DATA, path_RD_DATA)
+l_data <- regmatches(path_RD_DATA, r1)
 
 xml_data <- 
-  data.table(RD_OUT = unlist(L2), RD_data = list_with_na(L1))
+  data.table(RD_COMP = list
+             RD_OUT = unlist(L2), 
+             RD_data = list_with_na(L1))
 
 xml_data
 
@@ -38,6 +55,8 @@ trash <-
 tn <- 
   xml_text(xml_find_all(raw_xml, "//NAME"))
 
+tn
+length(tn)
 text_NAME <- tn[tn != ""]
 
 rep_sizes <- 
@@ -50,26 +69,7 @@ NAME_list <-
            rep_sizes)
     )
 
+length(NAME_list)
 xml_data[, NAME := NAME_list]
+xml_data[, TITLE := xml_text(xml_find_all(db, "//TITLE"))]
 xml_data
-
-  xml_find_all(db, "//TITLE")
-xml_attr
-xml_path(y)[1]
-
-
-gsub(p_RD_DATA, "\\1", xml_path(y))
-
-
-which(is.na(L1))
-
-lapply(), 
-       function(x){print(x) xml_attr(y,x))})
-
-
-
-list_with_na <- 
-  function(L1){
-     unlist(lapply(L1, function(x) ifelse(is.null(x), NA, x)))
-  }
-
