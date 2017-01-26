e82e2d0af74b1b8c0e6a2281d586531efa485cd1
jonatas <jonataseduardo@gmail.com>
2017-01-26 09:28:40 -0200

    starting to organize the code

--- src/xml_parser.R
+++ src/xml_parser.R
@@ -2,45 +2,29 @@ library(data.table)
 library(xml2)
 
 
-pg <- read_xml("http://www.ggobi.org/book/data/olive.xml")
+data_xml <- 
+  read_xml("../data/CD000004RawData.xml")
 
-db <- read_xml("../data/CD000004RawData.xml")
+RD_DATA <- xml_find_all(db, "//RD_DATA")
+RD_OUT <- xml_find_all(db, "//RD_OUT")
 
+p_RD_OUT <- "RD_OUT\\[([0-9]+)\\]"
+p_RD_DATA <- "(RD_DATA\\[)([0-9]+)(\\]$)"
 
-db
+path_RD_DATA <- xml_path(RD_DATA)
 
-?xml_find_all
-recs <- xml_find_all(pg, "//record")
+r1 <- gregexpr(p_RD_DATA, path_RD_DATA)
+L1 <-   regmatches(path_RD_DATA, r1)
 
-vals <- trimws(xml_text(recs))
-
-labs <- trimws(xml_attr(recs, "label"))
-
-?xml_attr
-  cols <- 
-    xml_attr(
-   xml_find_all(
-    pg, 
-    "//data/variables/*[self::categoricalvariable or self::realvariable]"
-    ),
-   "name")
-
-dat <- do.call(rbind(lapply(strsplit(vals, "\ +"), 
-                            function(x){
-                              data.frame(rbind(setNames(as.numeric(x), cols)))
-                            }))
-?xml_cdata 
-xml_text(xml_children(db)[2])
-
-xml_find_all(x, "//@x")
-xml_structure(db)
-
-y <- xml_find_all(db, "//RD_DATA")
-xml_path(y)
+r2 <- gregexpr(p_RD_OUT, path_RD_DATA)
+L2 <-   regmatches(path_RD_DATA, r1)
 
 xml_ns(db)
 
-xml_attr(y, 'STUDY')
+coln <- "TMEAN"
+length(xml_attr(y,coln ))
+xml_attr(y,coln )
+
 xml_attr(y, 'STUDY_ID')
 
 x <- 
@@ -50,17 +34,20 @@ help(package = 'xml2')
 xml_attr
 xml_path(y)[1]
 
-p_RD_OUT <- "RD_OUT\\[([0-9]+)\\]"
-p_RD_DATA <- "(RD_DATA\\[)([0-9]+)(\\]$)"
 
 gsub(p_RD_DATA, "\\1", xml_path(y))
 
-xml_path(y) 
-m <- gregexpr(p_RD_DATA, xml_path(y))
-  L1 <-   regmatches(xml_path(y), m)
 
 which(is.na(L1))
-?unlist
-L2 <- as.numeric(L1)
- unlist(lapply(L1, function(x) ifelse(is.null(x), NA, x)))
+
+lapply(unique(unlist(lapply(xml_attrs(y), names))), 
+       function(x){print(x) xml_attr(y,x))})
+
+z <-  data.table(RD_OUT = unlist(L2), RD_data = list_with_na(L1))
+
+
+list_with_na <- 
+  function(L1){
+     unlist(lapply(L1, function(x) ifelse(is.null(x), NA, x)))
+  }
 
